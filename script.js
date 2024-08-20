d5e8e97e382f6888751180cef1163fa1903a7e95
unknown <dannypratamaf@gmail.com>
2024-08-21 01:31:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//
+function btnHide() {
+  var x = document.getElementById("btnHide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function getColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setColor() {
+  document.documentElement.style.setProperty("--randomcolor", getColor());
+}
+setColor();
